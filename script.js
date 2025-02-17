3209e0d7ff7ca6b63ed8853e345b460b88ca5c34
Amit <81560269+immeas@users.noreply.github.com>
2025-02-17 09:14:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+let draggedElement = null;
+
+document.querySelectorAll('.image').forEach(box => {
+    box.addEventListener('dragstart', function (e) {
+        draggedElement = this;
+        this.classList.add("dragging");
+    });
+
+    box.addEventListener('dragend', function (e) {
+        this.classList.remove("dragging");
+    });
+
+    box.addEventListener('dragover', function (e) {
+        e.preventDefault();
+    });
+
+    box.addEventListener('drop', function () {
+        if (draggedElement !== this) {
+            let tempBg = this.style.backgroundImage;
+            this.style.backgroundImage = draggedElement.style.backgroundImage;
+            draggedElement.style.backgroundImage = tempBg;
+        }
+    });
+});
